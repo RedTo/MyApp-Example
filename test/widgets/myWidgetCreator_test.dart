19dc49c6a5ae57cf8da7bd4d332a1045334e57e2
Tobias Redl <huabatobs@gmail.com>
2019-04-20 19:54:15 +0200

    added test for genericSpinner

---
+++ test/widgets/myWidgetCreator_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myapp/widgets/ThickSeparator.dart';
+import 'package:myapp/widgets/myWidgetCreator.dart';
+import 'package:progress_hud/progress_hud.dart';
+
+import '../test_helper.dart';
+
+void main() {
+  testWidgets('myWidgetCreator genericSpinner', (WidgetTester tester) async {
+    await tester.pumpWidget(TestHelper.buildPage(MyWidgetCreator.buildGenericSpinner()));
+
+    expect(find.byType(ProgressHUD), findsOneWidget);
+    expect(find.text('Refreshing...'), findsOneWidget);
+  });
+
+  testWidgets('myWidgetCreator genericSpinner custom text', (WidgetTester tester) async {
+    await tester.pumpWidget(TestHelper.buildPage(MyWidgetCreator.buildGenericSpinner('Custom Text')));
+
+    expect(find.byType(ProgressHUD), findsOneWidget);
+    expect(find.text('Custom Text'), findsOneWidget);
+    expect(find.text('Refreshing...'), findsNothing);
+  });
+}
